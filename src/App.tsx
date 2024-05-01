83c2c0239f46172a059151be5cc042090b39ab77
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-05-01 19:00:44 +0530

    UseRef Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -16,3 +16,4 @@ export default App;
 
 
 //Reusablity and avoid code duplicacy
+//A context always need to be created outside the componsent, while its future value will always be set inside the component
---
+++ src/Components/ref/MutableRef.tsx
@@ -0,0 +1,31 @@
+import {useState, useRef, useEffect, Fragment} from "react";
+import {Button} from "../Button";
+
+export const MutableRef = () => {
+    const [timer, setTimer] = useState(0);
+    const interValRef = useRef<number|null>(null)
+
+    const stopTimer = () => {
+        if(interValRef.current)
+        window.clearInterval(interValRef.current)
+    }
+
+    useEffect(() => {
+        interValRef.current = window.setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+        return () => {
+            stopTimer()
+        }
+    }, [])
+
+    return (
+        <div>
+            HookTimer - {timer} -
+            <button onClick={() => stopTimer()}>Stop Timer</button>
+        </div>
+    )
+
+}
+
+//we use ref for a Dom Element or as a mutable value which works like a ref
