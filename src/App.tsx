8658b08e82863ad62590abe8a252a967d2e3e52a
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-05-01 10:33:58 +0530

    Event Listner props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Button} from "./Components/Button";
-import {Input} from "./Components/Input";
 import {Container} from "./Components/Container";
 
 function App() {
-
-
   return (
     <div className="App">
         <Container styles={{border:'1px solid red',padding:''}}/>
     </div>
   );
 }
-
 export default App;
+
+
+//Reusablity and avoid code duplicacy
