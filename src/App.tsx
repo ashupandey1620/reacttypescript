78c47d2a632ef7b200443f2903877c010ac5ecb1
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-05-01 10:29:12 +0530

    Event Listner props

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.css';
 import {Button} from "./Components/Button";
 import {Input} from "./Components/Input";
+import {Container} from "./Components/Container";
 
 function App() {
 
 
   return (
     <div className="App">
-      <Button handleClick={(event,id)=>{
-          console.log("Button Clicked",event);
-      }}
-      />
-        <Input value='' handlechange={(event)=>console.log(event)}/>
+        <Container styles={{border:'1px solid red',padding:''}}/>
     </div>
   );
 }
